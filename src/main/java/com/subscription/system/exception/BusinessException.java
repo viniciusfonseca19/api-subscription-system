95a12be63fce7fa399b1f3a9855fd26c9decf537
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-23 19:12:46 -0300

    teste com businessexception

--- src/main/java/com/subscription/system/exception/BusinessException.java
+++ src/main/java/com/subscription/system/exception/BusinessException.java
@@ -1,4 +1,8 @@
 package com.subscription.system.exception;
 
-public class BusinessException {
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
 }
