fb9bd504522dd5202e570d3c75f2e8788b1c1ba2
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-23 19:23:10 -0300

    response

--- src/main/java/com/subscription/system/dto/response/AuthResponse.java
+++ src/main/java/com/subscription/system/dto/response/AuthResponse.java
@@ -1,4 +1,14 @@
 package com.subscription.system.dto.response;
 
 public class AuthResponse {
+
+    private Long userId;
+    private String name;
+    private String email;
+
+    public AuthResponse(Long userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
 }
