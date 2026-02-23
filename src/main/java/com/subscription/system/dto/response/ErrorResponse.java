9e4581f0d6242dea85d227310c1a65813316fc20
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-23 19:15:46 -0300

    Criação do arquivo ErrorResponse

---
+++ src/main/java/com/subscription/system/dto/response/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.subscription.system.dto.response;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorResponse(LocalDateTime timestamp, int status,
+                         String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    // getters
+}
