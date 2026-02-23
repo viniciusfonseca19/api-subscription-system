beb4b6860a6ef7f71d1936ba8fdffa7ecdf87537
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-23 12:18:49 -0300

    Conexão com o banco de dados!

--- src/main/java/com/subscription/system/SubscriptionApplication.java
+++ src/main/java/com/subscription/system/SubscriptionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SystemApplication {
+public class SubscriptionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SystemApplication.class, args);
+		SpringApplication.run(SubscriptionApplication.class, args);
 	}
 
 }
