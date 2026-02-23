b889889c2d28c351289497168c2b845061fc82af
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-23 19:13:28 -0300

    extends no projeto de api

--- src/main/java/com/subscription/system/exception/ResourceNotFoundException.java
+++ src/main/java/com/subscription/system/exception/ResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package com.subscription.system.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
