a0fddad3f04238561ff42fc419a23bfa1c872c64
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-23 19:14:51 -0300

    possível finalização dos exceptions

---
+++ src/main/java/com/subscription/system/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.subscription.system.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
