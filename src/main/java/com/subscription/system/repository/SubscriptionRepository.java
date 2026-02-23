bce4ed4013fa2a71a876f6c4a93b7a27f4733e21
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-23 19:03:08 -0300

    Inicialização da pasta repository!

--- src/main/java/com/subscription/system/repository/SubscriptionRepository.java
+++ src/main/java/com/subscription/system/repository/SubscriptionRepository.java
@@ -1,4 +1,17 @@
 package com.subscription.system.repository;
 
-public interface SubscriptionRepository {
+import com.subscription.system.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    boolean existsByUserAndStatus(User user, SubscriptionStatus status);
+
+    List<Subscription> findByStatusAndExpirationDateBefore(
+            SubscriptionStatus status,
+            LocalDateTime date
+    );
 }
--- src/main/java/com/subscription/system/repository/UserRepository.java
+++ src/main/java/com/subscription/system/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.subscription.system.repository;
 
-public interface UserRepository {
+import com.subscription.system.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
